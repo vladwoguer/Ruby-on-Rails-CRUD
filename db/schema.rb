97bb4423c0bcfa2916d58f8abea91c8bee997f7e
Vladwoguer Bezerra <vladwoguerbezerra@gmail.com>
2017-06-12 22:59:43 -0300

    Pagination on templates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170611145422) do
+ActiveRecord::Schema.define(version: 20170613015511) do
 
   create_table "clientes", force: :cascade do |t|
     t.string "nome"
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 20170611145422) do
     t.date "nascimento"
     t.datetime "cadastro"
     t.float "altura"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "menus", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
